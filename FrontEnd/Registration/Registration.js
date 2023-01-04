c7fce47367344a9108219cdb474e3fc2a1d5a520
unknown <you@example.com>
2023-01-04 14:34:35 +0530

    Updated

---
+++ FrontEnd/Registration/Registration.js
@@ -0,0 +1,151 @@
+import React from 'react'
+import './Registration.css'
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import MenuItem from "@mui/material/MenuItem";
+import { TextField } from "@mui/material";
+import { useState } from "react";
+const Registration = ({ postData, setPostData }) => {
+
+	const [bloodgroup, setBloodgroup] = useState("")
+	const [status, setStatus] = useState("")
+
+	const handleSubmit = async (event) => {
+
+	};
+
+
+	return (
+		<Form onSubmit={handleSubmit}>
+
+			<h1 className='heading1'>DONOR REGISTRATION</h1>
+			<Form.Group className="mb-3">
+				<Form.Label></Form.Label>
+				<Form.Control
+					name="name"
+					type="text"
+					placeholder="Name / Organization"
+					onChange={(e) =>
+						setPostData({ ...postData, name: e.target.value })
+					}
+				/>
+			</Form.Group>
+
+
+
+
+
+			<Form.Group className="mb-3">
+				<Form.Label></Form.Label>
+				<br></br>
+				<Form.Control
+					name="password"
+					type="password"
+					placeholder="Password"
+					onChange={(e) => {
+						setPostData({ ...postData, password: e.target.value })
+
+					}
+
+					}
+				/>
+			</Form.Group>
+			<Form.Group className="mb-3">
+				<Form.Label></Form.Label>
+				<Form.Control
+					name="age"
+					type="number"
+					placeholder="age"
+					onChange={(e) =>
+						setPostData({ ...postData, age: e.target.value })
+					}
+				/>
+			</Form.Group>
+			<TextField
+				className="dropdown"
+				select
+				value={bloodgroup}
+				label="Blood Group"
+				onChange={(e) => {
+					setBloodgroup(e.target.value)
+					setPostData({ ...postData, bloodgroup: bloodgroup })
+				}
+				}
+			>
+				<MenuItem key="O+ve" value="O+ve">O+ve</MenuItem>
+				<MenuItem key="A+ve" value="A+ve">A+ve</MenuItem>
+				<MenuItem key="B+ve" value="B+ve">B+ve</MenuItem>
+				<MenuItem key="AB+ve" value="AB+ve">AB+ve</MenuItem>
+				<MenuItem key="O-ve" value="O-ve">O-ve</MenuItem>
+				<MenuItem key="A-ve" value="A-ve">A-ve</MenuItem>
+				<MenuItem key="B-ve" value="B-ve">B-ve</MenuItem>
+				<MenuItem key="AB-ve" value="AB-ve">AB-ve</MenuItem>
+			</TextField>
+			<Form.Group className="mb-3">
+				<Form.Label></Form.Label>
+				<Form.Control
+					name="contactnumber"
+					type="text"
+					placeholder="Contact Number"
+					onChange={(e) =>
+						setPostData({ ...postData, contactnumber: e.target.value })
+					}
+				/>
+			</Form.Group>
+			<Form.Group className="mb-3">
+				<Form.Label></Form.Label>
+				<Form.Control
+					name="height"
+					type="text"
+					placeholder="height"
+					onChange={(e) =>
+						setPostData({ ...postData, height: e.target.value })
+					}
+				/>
+			</Form.Group>
+			<Form.Group className="mb-3">
+				<Form.Label></Form.Label>
+				<Form.Control
+					name="weight"
+					type="text"
+					placeholder="weight"
+					onChange={(e) =>
+						setPostData({ ...postData, weight: e.target.value })
+					}
+				/>
+			</Form.Group>
+			<TextField
+				className="dropdown"
+				label="Last Donation"
+				type="date"
+				defaultValue=""
+				InputLabelProps={{
+					shrink: true,
+				}}
+				onChange={(e) =>
+					setPostData({ ...postData, date: e.target.value })
+				}
+			/><br></br>
+			<TextField
+				className="dropdown"
+				select
+				label="status"
+				value={status}
+				onChange={(e) => {
+					setStatus(e.target.value)
+					setPostData({ ...postData, status: status })
+				}
+				}
+			>
+				<MenuItem key="success" value="success">Success</MenuItem>
+				<MenuItem key="failed" value="failed">Failed</MenuItem>
+			</TextField>
+
+			<Button className="btn" variant="primary" type="submit">
+				Sign Up
+			</Button>
+		</Form>
+	)
+}
+
+export default Registration
--- FrontEnd/login/Login.jsx
+++ FrontEnd/login/Login.jsx
@@ -8,7 +8,7 @@
                 <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
                 <style dangerouslySetInnerHTML={{ __html: "\n        body {\n            font-family: 'Poppins';\n        }\n    " }} />
                 <style dangerouslySetInnerHTML={{ __html: "   \n\nbutton {   \n       background-color: #FF3838;   \n       width: 100%;  \n        color: whitesmoke;   \n        padding: 15px;   \n        margin: 10px 0px;   \n        border: none;   \n        cursor: pointer;   \n         }  \n\n form {   \n        border: 3px solid #f1f1f1; \n    }   \n input[type=text], input[type=password] {   \n        width: 100%;   \n        margin: 8px 0;  \n        padding: 12px 20px;   \n        display: inline-block;   \n        border: 2px orangered;   \n        box-sizing: border-box;   \n    }  \n button:hover {   \n        opacity: 0.7;   \n    }   \n  .cancelbtn {   \n        width: auto;   \n        padding: 10px 18px;  \n        margin: 10px 5px;  \n    }   \n        \n     \n .container { \n        padding: 50px;   \n        background-color: antiquewhite;\n          \n    }   \n" }} />
-                    <div>
+             
                     <img className='image' src={img} />
                     <form>
                         <div className="container">
@@ -33,7 +33,7 @@
                         <label>Don't have an account?</label>
                         <a href="#"> Sign Up
                         </a></div><a href="#">
-                </a></form></div><a href="#">
+                </a></form><a href="#">
                 </a></div >
       );
     }
